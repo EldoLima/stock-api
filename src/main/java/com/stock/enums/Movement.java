bcb5d4eefc1d8bd957f95eb69f3e35e5a56f1e1f
EldoLima <eldosilva906@gmail.com>
2026-03-05 22:32:34 -0300

    fix(movement): remove class not used

--- src/main/java/com/stock/enums/Movement.java
+++ src/main/java/com/stock/enums/Movement.java
@@ -1,4 +1,23 @@
 package com.stock.enums;
 
-public record Movement(String INBOUND, String OUTBOUND) {
+public enum Movement {
+    STOCKIN(1),
+    STOCKOUT(2);
+    private int code;
+
+    private Movement(int code) {
+        this.code = code;
+    }
+    public int getCode(){
+        return code;
+    }
+    public static Movement valueOf(int code){
+        for(Movement value : Movement.values()){
+            if(code == value.getCode()){
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Illegal Argument code");
+    }
+
 }
