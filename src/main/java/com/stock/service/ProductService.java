df66c19ded03f8f01bd7142f811719e36ba1ab8d
EldoLima <eldosilva906@gmail.com>
2026-03-05 22:04:00 -0300

    feat(product): implement logic in service

--- src/main/java/com/stock/service/ProductService.java
+++ src/main/java/com/stock/service/ProductService.java
@@ -3,6 +3,7 @@ package com.stock.service;
 
 import com.stock.model.entity.Product;
 import com.stock.repository.ProductRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,6 +23,29 @@ public class ProductService {
     public Product save(Product product){
        return repository.save(product);
     }
+
+    @Transactional
+    public void updateName(Product product, Long id){
+
+        Product productExisting = repository.findById(id).orElseThrow();
+        productExisting.setName(product.getName());
+        productExisting.setTotalProduct(product.getTotalProduct());
+        productExisting.setPrice(product.getPrice());
+        productExisting.setDescription(product.getDescription());
+        productExisting.setSKU(product.getSKU());
+        productExisting.setQtdArea(product.getQtdArea());
+        productExisting.setQuantityStock(product.getQuantityStock());
+        repository.save(productExisting);
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Product product1 = repository.findById(id).orElseThrow();
+
+        repository.delete(product1);
+
+    }
+
 
 
 
